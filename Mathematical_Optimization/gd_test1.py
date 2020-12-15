c5693639f92f581ae26d3db24deb977bab2d8bbb
yu-ololo <43878640+yu-ololo@users.noreply.github.com>
2020-12-16 02:34:28 +0900

    study GradientDescent
    模写

---
+++ Mathematical_Optimization/gd_test1.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import gridient_descent as gd
+
+def f(xx):
+    x = xx[0]
+    y = xx[1]
+    return 5 * x**2 - 6 * x * y + 3 * y**2 + 6 * x - 6 * y
+
+def df(xx):
+    x = xx[0]
+    y = xx[1]
+    return np.array([10 * x - 6 * y + 6, -6 * x + 6 * y -6])
+
+alpha=0.01
+algo = gd.GradientDescent(f, df, alpha)
+initial = np.array([1,1])
+algo.solve(initial)
+print(algo.x_)
+print(algo.opt_)
+
+
+plt.scatter(initial[0], initial[1], color="k", marker="o")
+plt.plot(algo.path_[:, 0], algo.path_[:, 1], color="k", linewidth=1.5)
+
+xs = np.linspace(-2, 2, 300)
+ys = np.linspace(-2, 2, 300)
+xmesh, ymesh = np.meshgrid(xs, ys)
+xx = np.r_[xmesh.reshape(1, -1), ymesh.reshape(1, -1)]
+levels = [-3, -2.9, -2.8, -2.6, -2.4, -2.2, -2, -1, 0, 1, 2, 3, 4]
+
+plt.contour(xs, ys, f(xx).reshape(xmesh.shape), levels=levels,
+            colors="k", linestyles="dotted")
+
+plt.show()
